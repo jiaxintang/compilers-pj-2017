b9daf6344441db07d67355a03a21f27e95ec0453
Jiaxin <15121008171@163.com>
2017-12-07 19:48:13 +0800

    12.07 add a sample minijava program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String sentence = "{213,23}";
-        ANTLRInputStream in = new ANTLRInputStream(sentence);
+        ANTLRInputStream in = new ANTLRInputStream(args[0]);
         MiniJavaLexer lexer = new MiniJavaLexer(in);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         MiniJavaParser parser = new MiniJavaParser(tokens);
