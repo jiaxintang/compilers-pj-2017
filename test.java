c2af812c779ec414fcb7e714a04d3db85e0b9cd6
cutrain <454615501@qq.com>
2017-12-30 22:06:56 +0800

    autocommit

---
+++ test.java
@@ -0,0 +1,16 @@
+class test{
+	public static void main(String[] a){
+		System.out.println(new Fuck().Haha(10));
+	}
+}
+
+class Fuck {
+	public int Haha(int num) {
+		int n;
+		int n;
+		m = n + 1 * 2;
+		return m;
+	}
+
+}
+
